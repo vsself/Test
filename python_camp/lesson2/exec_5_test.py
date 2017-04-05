6ada30a1bc5ed9daeb1fe66e117fad0b4cb8de44
Zheng <longfei.zheng@nokia.com>
2017-04-05 15:05:37 +0800

    delete lesson 2

--- python_camp/lesson2/exec_5_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import exec_5
-
-class TestFactorial(unittest.TestCase):
-    def test_factorial_case_1(self):
-        result = exec_5.factorial(5)
-        self.assertEqual(result, 120)
